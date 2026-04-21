2b339bf74be08b59b4fe0c9d2f48c668898c11bf
alibekorazaly <alibek.orazaly1012@gmail.com>
2026-04-21 22:12:11 +0500

    Searcher created

--- src/Searcher.java
+++ src/Searcher.java
@@ -1,7 +1,22 @@
 package src;
 
 public class Searcher {
-    int search(int[] arr, int target) {
-        return 0;
+
+    public int search(int[] arr, int target) {
+        int left = 0;
+        int right = arr.length - 1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
     }
 }
