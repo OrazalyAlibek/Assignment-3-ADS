bd97bed07365236a132d2c329c4d94f34b7f2442
alibekorazaly <alibek.orazaly1012@gmail.com>
2026-04-21 21:29:34 +0500

    Sorter created

--- src/Sorter.java
+++ src/Sorter.java
@@ -1,10 +1,76 @@
 package src;
 
+import java.util.Random;
+
 public class Sorter {
-    public void basicSort(int[] arr) {}
-    public void advancedSort(int[] arr) {}
-    private void mergeSort(int[] arr, int left, int right) {}
-    private void merge(int[] arr, int left, int mid, int right) {}
-    public void printArray(int[] arr) {}
-    public void generateRandomArray(int size) {}
+
+    public void basicSort(int[] arr) {
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public void advancedSort(int[] arr) {
+        if (arr.length <= 1) {
+            return;
+        }
+
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        for (int i = 0; i < mid; i++) {
+            left[i] = arr[i];
+        }
+        for (int i = mid; i < arr.length; i++) {
+            right[i - mid] = arr[i];
+        }
+
+        advancedSort(left);
+        advancedSort(right);
+
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                arr[k] = left[i];
+                i++;
+            } else {
+                arr[k] = right[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < left.length) {
+            arr[k] = left[i]; i++; k++;
+        }
+        while (j < right.length) {
+            arr[k] = right[j]; j++; k++;
+        }
+    }
+
+    public void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) {
+                System.out.print(", ");
+            }
+        }
+    }
+
+    public int[] generateRandomArray(int size) {
+        Random random = new Random();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(10000);
+        }
+        return arr;
+    }
 }
